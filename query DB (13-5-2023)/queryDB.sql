ef35de26a043d7e5c2e9b3b3dc64b8f74acdfc92
Fedekh <federicocet@gmail.com>
2023-05-12 13:01:36 +0200

    conclude query 5

--- query DB (13-5-2023)/queryDB.sql
+++ query DB (13-5-2023)/queryDB.sql
@@ -18,8 +18,17 @@ WHERE `cfu` > 10;
 
 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
+SELECT `name`, `period`,`year`
+FROM `courses` 
+WHERE `year` = 1 AND `period` = 'I semestre';
+
 
 5. Selezionare tutti gli appelli d esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+
+SELECT `course_id`
+FROM `exams`
+WHERE `hour` > "14%" 
+AND `date` = "2020-06-20";
 
 
 6. Selezionare tutti i corsi di laurea magistrale (38)
