2f6ede5d6f0fbf7be9556fe53a4ee20b11e41581
Fedekh <federicocet@gmail.com>
2023-05-15 17:06:54 +0200

    add query n 5 exercise JOIN

--- Query db 15-05-2023/EX - Query con JOIN.sql
+++ Query db 15-05-2023/EX - Query con JOIN.sql
@@ -44,6 +44,16 @@ ORDER BY `students`.`name` ASC, `students`.`surname` ASC;
 
 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
+SELECT `degrees`.`name`, `courses`.`name`, `teachers`.`name`
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`;
+
+
 
 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
