b1a0cc061549e12bdbbae1675e1b1b1cceab657c
Fedekh <federicocet@gmail.com>
2023-05-15 15:54:51 +0200

    Conclude all query from 1 to 4

--- Query db 15-05-2023/EX - Query con GROUP BY.sql
+++ Query db 15-05-2023/EX - Query con GROUP BY.sql
@@ -13,5 +13,15 @@ GROUP BY `office_address`
 
 3. Calcolare la media dei voti di ogni appello d esame
 
+SELECT `exam_id`, AVG(`vote`) 
+FROM `exam_student` 
+GROUP BY `exam_id`
+
 
 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT `departments`.`name`, COUNT(`degrees`.`name`) AS `total_courses`
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+GROUP BY `departments`.`name`
