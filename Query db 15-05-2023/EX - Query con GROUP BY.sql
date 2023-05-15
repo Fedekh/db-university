86ee6ffef332e70b7b0af805274ac37834401d0d
Fedekh <federicocet@gmail.com>
2023-05-15 14:20:37 +0200

    Conclude query 1 and 2 exercise query group by

---
+++ Query db 15-05-2023/EX - Query con GROUP BY.sql
@@ -0,0 +1,17 @@
+1. Contare quanti iscritti ci sono stati ogni anno
+
+SELECT COUNT(`id`) AS `number_student_foryear`, YEAR(`enrolment_date`) AS `Year`
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
+
+2. Contare gli insegnanti che hanno l ufficio nello stesso edificio
+
+SELECT COUNT(`id`) AS `number_teacher_sameaddress`, `office_address`
+FROM `teachers` 
+GROUP BY `office_address`
+
+3. Calcolare la media dei voti di ogni appello d esame
+
+
+4. Contare quanti corsi di laurea ci sono per ogni dipartimento
